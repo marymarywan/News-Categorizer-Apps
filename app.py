141cc7753223c49d602ce6861750e0617decbca7
Marwan Yeedeng <watchdog.av@gmail.com>
2022-04-17 11:59:48 +0700

    update

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, render_template, redirect, url_for
 import pickle
-import mypackage.demo
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST'])
 def predict():
     if request.method == 'POST':
         user = request.form["nm"]
